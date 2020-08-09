f07918f3dcfb22417a32f18411ad9f8905e0101f
nicolas <nicolas.eymenier@gmail.com>
2020-08-09 16:45:49 -0300

    created more ingredients and modified the navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.delete_all
+CakeIngredient.delete_all
+Ingredient.delete_all
 IngredientCategory.delete_all
 
 
@@ -20,10 +22,37 @@ user = User.new(
 
 user.save!
 
-IngredientCategory.create(name: "Massa", how_many_items: 1)
-IngredientCategory.create(name: "Recheio", how_many_items: 2)
-IngredientCategory.create(name: "Cobertura", how_many_items: 2)
-IngredientCategory.create(name: "Decoração",how_many_items: 3)
-IngredientCategory.create(name: "Formato do bolo", how_many_items: 1)
+dough = IngredientCategory.create(name: "Massa", how_many_items: 1)
+filling = IngredientCategory.create(name: "Recheio", how_many_items: 2)
+icing = IngredientCategory.create(name: "Cobertura", how_many_items: 2)
+decoration = IngredientCategory.create(name: "Decoração",how_many_items: 3)
+shape = IngredientCategory.create(name: "Formato do bolo", how_many_items: 1)
 
 
+Ingredient.create(name: "Chocolate", ingredient_category: dough)
+Ingredient.create(name: "Chocolate branco", ingredient_category: dough)
+Ingredient.create(name: "Baunilha", ingredient_category: dough)
+Ingredient.create(name: "Cremosa", ingredient_category: dough)
+Ingredient.create(name: "Especial", ingredient_category: dough)
+
+Ingredient.create(name: "Morango", ingredient_category: filling)
+Ingredient.create(name: "Doce de leite", ingredient_category: filling)
+Ingredient.create(name: "Damasco", ingredient_category: filling)
+Ingredient.create(name: "Petit Gateau", ingredient_category: filling)
+Ingredient.create(name: "Nutella", ingredient_category: filling)
+Ingredient.create(name: "Manga", ingredient_category: filling)
+Ingredient.create(name: "Especial La'Docura", ingredient_category: filling)
+
+Ingredient.create(name: "Açucar", ingredient_category: icing)
+Ingredient.create(name: "Chocolate Branco", ingredient_category: icing)
+Ingredient.create(name: "Mousse de Limão", ingredient_category: icing)
+Ingredient.create(name: "Côco", ingredient_category: icing)
+
+
+Ingredient.create(name: "Especial", ingredient_category: decoration)
+Ingredient.create(name: "Bonita", ingredient_category: decoration)
+Ingredient.create(name: "Verão", ingredient_category: decoration)
+
+Ingredient.create(name: "Quadrado", ingredient_category: shape)
+Ingredient.create(name: "Rodondo", ingredient_category: shape)
+Ingredient.create(name: "Triangulo", ingredient_category: shape)
