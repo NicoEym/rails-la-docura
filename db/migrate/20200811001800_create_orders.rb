10afedbc3bdef3320662071ac3b9cd996a4a6278
nicolas <nicolas.eymenier@gmail.com>
2020-08-10 21:19:21 -0300

    created models order and cart

--- db/migrate/20200811001800_create_orders.rb
+++ db/migrate/20200811001800_create_orders.rb
@@ -1,9 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.date :delivery_date
-      t.float :price
-      t.string :size
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
