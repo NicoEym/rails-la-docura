0f4abe17640810c3f6b873034fc6b4fa38977547
nicolas <nicolas.eymenier@gmail.com>
2020-08-14 19:45:50 -0300

    started to code the cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,17 @@ class ApplicationController < ActionController::Base
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 
+  def current_cart
+    if session[:cart_id]
+      @current_cart ||= Cart.find(session[:cart_id])
+    end
+    if session[:cart_id].nil?
+      @current_cart = Cart.create!
+      session[:cart_id] = @current_cart.id
+    end
+    @current_cart
+  end
+
   private
 
   def skip_pundit?
